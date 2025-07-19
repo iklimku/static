17f33216b830868f63f223781ef8b2245adeb1df
ArjunaWahyu <arjunawahyu0007@gmail.com>
2025-07-19 19:10:22 +0700

    Refactor MainTabs component and update layout for improved structure and readability; remove old Tabs component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { MainCarousel } from "@/components/organisms/MainCarousel";
 import { Sectorals } from "@/components/organisms/Sectorals";
 // import { Main } from "next/document";
-import { MainTabs } from "@/components/organisms/Tabs";
+import { MainTabs } from "@/components/organisms/MainTabs";
 
 export default function Home() {
   return (
     <>
       <main className="container mx-auto max-w-screen-lg">
         {/* Carousel */}
-        <div className="w-auto h-auto">
+        <div className="w-auto h-auto mx-4">
           <MainCarousel />
         </div>
 
@@ -16,7 +16,7 @@ export default function Home() {
         <div className="w-auto h-auto mt-9">
           <Sectorals />
         </div>
-        <div className="w-auto h-auto mt-9">
+        <div className="w-auto h-auto mt-9 mx-4">
           <MainTabs />
         </div>
       </main>

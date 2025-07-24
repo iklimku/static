a080056935dedc68077de4bc8a33c21cf3194232
ArjunaWahyu <arjunawahyu0007@gmail.com>
2025-07-24 14:46:04 +0700

    add data to daftar

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://cews.bmkg.go.id/**')]
+    remotePatterns: [
+      new URL('https://cews.bmkg.go.id/**'),
+      // https://i0.wp.com/
+      new URL('https://i0.wp.com/**'),
+      new URL('https://dataweb.bmkg.go.id/**'),
+    ]
   }
 };
 

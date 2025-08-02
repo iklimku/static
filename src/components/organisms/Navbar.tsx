5bbea27c1954d2b169299e2efdb65390916b95ac
ArjunaWahyu <arjunawahyu0007@gmail.com>
2025-08-02 16:03:16 +0700

    fix navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -67,7 +67,7 @@ export function Navbar() {
                     {data.title}
                   </NavigationMenuTrigger>
                   <NavigationMenuContent className="p-4 min-w-[400px] md:min-w-[500px] lg:min-w-[1000px] border shadow-lg bg-white rounded-md z-50 ">
-                    <ul className={`grid gap-1 md:grid-flow-col md:grid-rows-${Math.ceil(data.subMenu.length / 2)}`}>
+                    <ul className={`grid gap-1 grid-flow-col grid-rows-${Math.ceil(data.subMenu.length / 2)}`}>
                       {data.subMenu.map((item) => (
                         <ListItem
                           key={item.title}

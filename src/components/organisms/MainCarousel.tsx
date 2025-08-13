897b248a1a6ba62876641d06381254160d5d3c52
ArjunaWahyu <arjunawahyu0007@gmail.com>
2025-08-13 08:54:03 +0700

    refactor: enhance MainCarousel layout and functionality with improved state management

--- src/components/organisms/MainCarousel.tsx
+++ src/components/organisms/MainCarousel.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link"; // 1. Impor komponen Link
 import { Suspense } from "react";
+import { useState, useEffect } from "react";
 import Loading from "@/components/organisms/Loading";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
+  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
@@ -18,50 +20,80 @@ import Autoplay from "embla-carousel-autoplay";
 // 2. Impor data dari file JSON
 import carouselData from "@/../public/data/carousel.json";
 
+interface Data {
+  id: number;
+  url: string;
+  alt: string;
+  slug: string;
+  description: string;
+}
+
 export function MainCarousel() {
-  const images = carouselData; // Gunakan data dari file JSON
+  const datas: Data[] = carouselData; // Gunakan data dari file JSON
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    if (!api) {
+      return
+    }
+
+    setCurrent(api.selectedScrollSnap())
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap())
+    })
+  }, [api])
 
   return (
-    <div className="w-full px-4 sm:px-0">
-      <Carousel
-        plugins={[
-          Autoplay({
-            delay: 2000,
-          }),
-        ]}
-        className="relative w-full mx-auto mt-4 mb-4 overflow-hidden rounded-xl shadow-xl"
-      >
-        <CarouselContent className="h-full">
-          {images.map((img, index) => {
-            const isAnimatedGif = img.url.endsWith(".gif");
-            return (
-              // 3. Bungkus CarouselItem dengan komponen Link
-              <CarouselItem key={img.id} className="h-full">
-                <Link href={`/${img.slug}/detail`} passHref>
-                  <Card className="h-full cursor-pointer transition-transform duration-300 hover:scale-105">
-                    <CardContent className="p-0 relative h-[300px] sm:h-[400px] md:h-[500px]">
-                      <Suspense fallback={<Loading />}>
-                        <Image
-                          src={img.url}
-                          alt={img.alt}
-                          fill
-                          className="object-contain" // Hapus pointer-events-none
-                          sizes="(max-width: 768px) 100vw, 70vh"
-                          priority={index === 0}
-                          {...(isAnimatedGif && { unoptimized: true })}
-                        />
-                      </Suspense>
-                    </CardContent>
-                  </Card>
-                </Link>
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
+    // bagi dua layarnya
+    <div className="flex flex-col-reverse xl:flex-row w-full px-0 border border-black">
+      <div className="w-full text-center xl:w-1/3 border border-black">
+        <h2 className="text-lg font-bold">{ datas[current].alt }</h2>
+        <p>{ datas[current].description }</p>
+      </div>
+      <div className="w-full xl:w-2/3 border border-black">
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 5000,
+            }),
+          ]}
+          className="relative w-full mx-auto mt-4 mb-4 overflow-hidden rounded-xl shadow-xl"
+          setApi={setApi}
+        >
+          <CarouselContent className="h-full">
+            {datas.map((img, index) => {
+              const isAnimatedGif = img.url.endsWith(".gif");
+              return (
+                // 3. Bungkus CarouselItem dengan komponen Link
+                <CarouselItem key={img.id} className="h-full">
+                  <Link href={`/${img.slug}/detail`} passHref>
+                    <Card className="h-full cursor-pointer transition-transform duration-300 hover:scale-105">
+                      <CardContent className="p-0 relative h-[300px] sm:h-[400px] md:h-[500px]">
+                        <Suspense fallback={<Loading />}>
+                          <Image
+                            src={img.url}
+                            alt={img.alt}
+                            fill
+                            className="object-contain" // Hapus pointer-events-none
+                            sizes="(max-width: 768px) 100vw, 70vh"
+                            priority={index === 0}
+                            {...(isAnimatedGif && { unoptimized: true })}
+                          />
+                        </Suspense>
+                      </CardContent>
+                    </Card>
+                  </Link>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
 
-        <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white hover:bg-black/70 rounded-full p-2" />
-        <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white hover:bg-black/70 rounded-full p-2" />
-      </Carousel>
+          <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white hover:bg-black/70 rounded-full p-2" />
+          <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-black/50 text-white hover:bg-black/70 rounded-full p-2" />
+        </Carousel>
+      </div>
     </div>
   );
 }

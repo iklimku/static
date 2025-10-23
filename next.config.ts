c600b5511f3a3bc0044c904c1ed7035d9d0a56bc
digitradiart <digitradiart@gmail.com>
2025-10-23 14:58:49 +0700

    change fetch faq json to import

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ Tambahan: biar cocok dengan GitHub Pages
   output: "export",
   images: {
-    unoptimized: true, // GitHub Pages ga support Image Optimization
+    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cews.bmkg.go.id",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i0.wp.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dataweb.bmkg.go.id",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "cews.bmkg.go.id", pathname: "/**" },
+      { protocol: "https", hostname: "i0.wp.com", pathname: "/**" },
+      { protocol: "https", hostname: "dataweb.bmkg.go.id", pathname: "/**" },
+      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
     ],
   },
+  eslint: { ignoreDuringBuilds: true },
 
-  // ✅ Tambahan: abaikan error lint di build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // ✅ Tambahan opsional: kalau repo kamu bukan di root domain
-  // contoh: https://username.github.io/nama-repo/
-  // uncomment baris di bawah dan ganti nama-repo sesuai repo kamu:
   // basePath: "/static",
   // assetPrefix: "/static/",
 };
